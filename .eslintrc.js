bec709561ce11c7d3783c822c0b788d237a64518
2295790516@qq.com <2295790516@qq.com>
2018-08-03 17:59:06 +0800

    test shell

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
     ],
     // add your custom rules here
     rules: {
+
         // allow async-await
         'generator-star-spacing': 'off',
         // allow debugger during development
